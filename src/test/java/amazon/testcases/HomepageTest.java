477bbcc7257cdb00fc82e4ea789231fbe2f2a852
Arun-Prasanth-S <arunprasanths761.ss@gmail.com>
2023-10-25 15:22:49 +0530

    1st commit

---
+++ src/test/java/amazon/testcases/HomepageTest.java
@@ -0,0 +1,42 @@
+package amazon.testcases;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.microsoft.playwright.Page;
+
+import amazon.pages.HomePage;
+import baseplaywright.BaseClassPW;
+
+public class HomepageTest  {
+	HomePage homepage;
+	Page page;
+	BaseClassPW BC;
+	@BeforeMethod
+	public void frontPage() {
+		BC = new BaseClassPW();
+		page = BC.launchBrowser("chrome", "https://www.amazon.in/");
+		homepage = new HomePage(page);
+		int i = 0;
+		
+	}
+	
+	@Test
+	public void TC1() {
+		homepage.signInPage("Arun");
+		homepage.validate();
+	}
+	
+	@Test
+	public void TC2() {
+		homepage.searchFunction("oneplus earbuds");
+		homepage.productAddToCart();
+	}
+
+	@AfterTest
+	public void close() {
+		BC.closeAll();
+		BC.closePlaywright();
+	}
+}
